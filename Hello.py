e2cbd809f045b8e2855948a5010e8bf93b2828e2
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-05 19:08:12 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,27 +1,9 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-python -m pip install openai
+
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
 import openai
 import json
 import pandas as pd
-user_api_key = st.sidebar.text_input("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", type="password")
+user_api_key = st.sidebar.text_input("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")
 client = openai.OpenAI(api_key=user_api_key)
 
 prompt = """Act as an AI writing tutor in English. 
@@ -58,5 +40,3 @@ if st.button('Submit'):
     print(suggestion_df)
     st.table(suggestion_df)
 
-if __name__ == "__main__":
-    run()
