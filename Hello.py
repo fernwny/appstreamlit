f2da67ba5221dde2752d3a092a04e7d860955c4a
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 21:39:22 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ else:
         try:
             # Set the parameters
             response = openai.completions.create(
-                engine="text-davinci-002",
+                model ="text-davinci-002",
                 prompt=prompt + user_input,
                 temperature=0.7,
                 max_tokens=100,
