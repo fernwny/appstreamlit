e736495b351af2b4d6c09f1f0d8628c20bbf9692
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 20:02:58 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,24 +1,7 @@
 import streamlit as st
 import json
 import pandas as pd
-import os
-from openai import OpenAI
-
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
-
+import openai
 # Get OpenAI API key from the user
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
