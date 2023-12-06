ec233753ba5885c7628bccc078907b1fc37e5e14
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 21:33:33 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -30,12 +30,6 @@ else:
                     {"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": prompt + user_input},
                 ]
-                temperature=0.7,
-                max_tokens=100,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0.6,
-                stop=["\n", " Lyrics:", " Title:"]
             )
 
 
