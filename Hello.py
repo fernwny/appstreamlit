265e452d0da4d4f68946b95cd2fcce17ad3ce5c4
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 18:16:26 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,47 +1,42 @@
-
 import streamlit as st
 import openai
 import json
 import pandas as pd
-import time
 
-    
+# Get OpenAI API key from the user
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
 
 prompt = """Act as an AI bookseller in English. You will receive a book title or author name or a short description of the book
-and you should give a book title, a short description of the book and genre ,
- and a link to where it can be purchased and list the suggestions in a JSON array, one suggestion per line."""
+and you should give a book title, a short description of the book and genre, 
+and a link to where it can be purchased and list the suggestions in a JSON array, one suggestion per line."""
 
 st.title('Book seller')
 
+st.markdown('Input the book title or author or description of the book that you want to buy. \nThe AI will give you suggestions on how the book is.')
 
-st.markdown('Input the book title or author or description of book that you want to buy. \nThe AI will give you suggestions on how The book is.')
-
-
+# Get user input
 user_input = st.text_area("Enter some text to correct:", "Your text here")
 
+# Streamlit form for submission
+with st.form("my_form"):
+    st.button('Submit')
+    if st.form_submit_button("Submit"):
+        messages_so_far = [
+            {"role": "system", "content": prompt},
+            {'role': 'user', 'content': user_input},
+        ]
 
+        # Use the OpenAI client consistently
+        response = client.completions.create(
+            model="davinci",
+            messages=messages_so_far
+        )
 
-# submit button after text input
+        # Show the response from the AI in a box
+        st.markdown('**AI response:**')
+        suggestion_dictionary = response.choices[0].message.content
 
-if st.button('Submit'):
-    messages_so_far = [
-        {"role": "system", "content": prompt},
-        {'role': 'user', 'content': user_input},
-    ]
-    response = client.completions.create(
-        model="davinci",
-        messages=messages_so_far
-    )
-    # Show the response from the AI in a box
-    st.markdown('**AI response:**')
-    suggestion_dictionary = response.choices[0].message.content
-
-
-    sd = json.loads(suggestion_dictionary)
-
-    print (sd)
-    suggestion_df = pd.DataFrame.from_dict(sd)
-    print(suggestion_df)
-    st.table(suggestion_df)
+        sd = json.loads(suggestion_dictionary)
+        suggestion_df = pd.DataFrame.from_dict(sd)
+        st.table(suggestion_df)
