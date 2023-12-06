f2648fc218c154895e3336105fe3465c687bb4a8
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 20:41:36 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -25,13 +25,13 @@ else:
             # Set the parameters
     # Set the parameters
             response = openai.completions.create(engine="davinci",
-            prompt=prompt + user_input,
-            temperature=0.7,
-            max_tokens=100,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=["\n", " Lyrics:", " Title:"])
+                prompt=prompt + user_input,
+                temperature=0.7,
+                max_tokens=100,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0.6,
+                stop=["\n", " Lyrics:", " Title:"])
 
             # Check for API errors
             if "choices" in response and response["choices"]:
