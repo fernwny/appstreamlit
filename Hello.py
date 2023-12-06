df9b6aa4faf15cd06fa8955c9cf10465d42a9fdb
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-06 20:36:42 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -35,3 +35,23 @@ else:
             if "choices" in response and response["choices"]:
                 # Display the generated lyrics
                 st.subheader("Generated Lyrics")
+                # Display the generated lyrics
+                st.write(response["choices"][0]["text"])
+
+                # Display the generated lyrics in a file
+                st.subheader("Generated Lyrics in a File")
+                # Get the file name from the user
+                file_name = st.text_input("Enter the file name", "lyrics.txt")
+                # Check if the file name is provided
+                if file_name:
+                    # Save the generated lyrics in a file
+                    with open(file_name, "w") as f:
+                        f.write(response["choices"][0]["text"])
+                    # Display the file content
+                    with open(file_name, "r") as f:
+                        st.write(f.read())
+            else:
+                st.error(f"API Error: {response.get('error', {}).get('message', 'Unknown error')}")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
