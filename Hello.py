54cd25126dbf4447f3dffa8fc794e116d9e1b8a0
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-07 19:32:42 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -61,14 +61,19 @@ else:
                 # Display the response
                 st.markdown('**AI response:**')
                 # Extract the suggestions from the response
-                suggestion_dictionary = response['choices'][0]['text'].strip()
+                # Extract the suggestions from the response
+                suggestion_dictionary = response.choices[0].text.strip()
+                st.write("Suggestion Dictionary:")
+                st.text(suggestion_dictionary)
                 try:
                     sd = json.loads(suggestion_dictionary)
                 except json.JSONDecodeError:
                     st.error("The response does not contain valid JSON.")
                 else:
                     suggestion_df = pd.DataFrame.from_dict(sd)
+                    st.write("Suggestion DataFrame:")
                     st.table(suggestion_df)
+
             else:
                 st.error("No choices found in the OpenAI response.")
         except Exception as e:
