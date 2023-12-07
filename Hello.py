c0f07de78719c36c6f3c83a6b7a394e85107a98a
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-07 18:41:41 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,7 +12,7 @@ else:
     st.text("Entered API Key: ************" + user_api_key[-4:])
     # Set the OpenAI API key
     openai.api_key = user_api_key
-    prompt = """Act as an AI songwriter and generate lyrics for a song. You will type the key words then the AI will generate the lyrics for you
+    prompt = """Act as an AI songwriter and generate lyrics for a song. You will type the key words then the AI will generate the lyrics for you like this: keyword : love lyrics: i love you to the moon etc.
     The AI will generate the next 5 lines of the song, and you will choose the best one.
     Each line will be generated based on the previous line, and each line should have 10 words. The lyrics should have 3-5 verses and 3 choruses, and the song should have a title. """
     st.write("AUTO Songwriter You will type keywords and the AI will generate the lyrics for you.")
@@ -41,7 +41,7 @@ else:
             
 
                 st.write("Title:")
-                st.success(generated_text.split("Title:")[1].split("\n")[0])
+                st.success(generated_text)
             else:
                 st.error("No choices found in the OpenAI response.")
         except Exception as e:
