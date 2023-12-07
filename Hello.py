76324f491a58db078c3c346e9c6d4b2b021407d3
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-07 19:47:12 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,57 +1,52 @@
+
+
 import streamlit as st
 import openai
 import json
 import pandas as pd
 
-# Get OpenAI API key from the user
+# Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
-# Check if the API key is provided
-if not user_api_key:
-    st.warning("Please enter your OpenAI API key.")
-else:
-    # Display the entered API key securely
-    st.text("Entered API Key: ************" + user_api_key[-4:])
-    # Set the OpenAI API key
-    openai.api_key = user_api_key
+client = openai.OpenAI(api_key=user_api_key)
+prompt = """Act as an AI writing tutor in English. You will receive a 
+            piece of writing and you should give suggestions on how to improve it.
+            List the suggestions in a JSON array, one suggestion per line.
+            Each suggestion should have 3 fields:
+            - "before" - the text before the suggestion
+            - "after" - the text after the suggestion
+            - "category" - the category of the suggestion one of "grammar", "style", "word choice", "other"
+            - "comment" - a comment about the suggestion
+            Don't say anything at first. Wait for the user to say something.
+        """    
 
-    # Prompt for song lyrics
-    prompt = f"""
-    You are an AI songwriter. Generate lyrics for a song based on the user's input.
 
-    System: Act as an AI songwriter and generate lyrics for a song.
-    User: {user_input}
-    """
+st.title('Writing tutor')
+st.markdown('Input the writing that you want to improve. \n\
+            The AI will give you suggestions on how to improve it.')
 
-    # Get the user input
-    user_input = st.text_input("Enter the keywords for the song lyrics", "You text here")
+user_input = st.text_area("Enter some text to correct:", "Your text here")
 
-    # Check if the user input is provided
-    if not user_input:
-        st.warning("Please enter the keywords for the song lyrics.")
-    else:
-        try:
-            # Set the parameters
-            response = openai.completions.create(
-                engine="text-davinci-003",
-                prompt=prompt,
-                temperature=0.7,
-                max_tokens=300,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0.6,
-                stop=["\n", " Lyrics:", " Title:"]
-            )
 
-            # Check if the response is successful
-            if response and response.choices and response.choices[0].text.strip():
-                # Display the response
-                st.markdown('**AI response:**')
-                st.text(response.choices[0].text.strip())
-            else:
-                st.error("No valid choices found in the OpenAI response.")
-        except Exception as e:
-            # Handle any exception here
-            st.error(f"An error occurred: {e}")
+# submit button after text input
+if st.button('Submit'):
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far
+    )
+    # Show the response from the AI in a box
+    st.markdown('**AI response:**')
+    suggestion_dictionary = response.choices[0].message.content
 
-        st.write("Lyrics have been successfully generated.")
+
+    sd = json.loads(suggestion_dictionary)
+
+    print (sd)
+    suggestion_df = pd.DataFrame.from_dict(sd)
+    print(suggestion_df)
+    st.table(suggestion_df)
+
