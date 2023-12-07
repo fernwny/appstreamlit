5ace86e67ac8e44e13afa91bd03d2ef38a6b5a57
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-07 20:25:40 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,4 @@
 
-
 import streamlit as st
 import openai
 import json
@@ -9,26 +8,19 @@ import pandas as pd
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as an AI songwriter in English. You will receive text input from a user and you should respond with a song. The user will then rate your song and give you feedback.
- You should use this feedback to improve your song. You will be scored on how well you can improve your song. The better you are at improving your song, the higher your score will be.
- list the songs you have written so far in a JSON array. Each song should be a JSON object with the following fields:
-    - song: the song you wrote
-    - rating: the rating the user gave your song
-    - feedback: the feedback the user gave your song
-    - score: the score you received for this song
-    - suggestions: the suggestions the user gave you for this song
+prompt = """Act as a lyricist and write a song about a topic of your choice. The song should be at least 4 lines long.
+list the topic, lyric and vocabulary of the song in a JSON array.
+-first line should be the topic
+-second line should be the lyric
+-third line should be the vocabulary"""
+
+st.title("Lyricist")
+st.markdown("This app uses the OpenAI API to generate lyrics based on a topic of your choice.")
+
+# Get the topic from the user
+topic = st.text_input("Topic", "ENTER TOPIC HERE")
 
 
-
-"""
-st.title('Writing song')
-st.markdown('Input the writing that you want to improve. \n\
-            The AI will give you suggestions on how to improve it.')
-
-user_input = st.text_area("Enter some text to correct:", "Your text here")
-
-
-# submit button after text input
 if st.button('Submit'):
     messages_so_far = [
         {"role": "system", "content": prompt},
@@ -38,8 +30,9 @@ if st.button('Submit'):
         model="gpt-3.5-turbo",
         messages=messages_so_far
     )
-    # Show the response from the AI in a box
-    st.markdown('**AI response:**')
+    # Show the lyrics to the user
+    st.markdown('**Lyrics:**')
+    st.write(response.choices[0].text)
     suggestion_dictionary = response.choices[0].message.content
 
 
@@ -49,4 +42,3 @@ if st.button('Submit'):
     suggestion_df = pd.DataFrame.from_dict(sd)
     print(suggestion_df)
     st.table(suggestion_df)
-
