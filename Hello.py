1adb8f53438500e530f199f57a311340652d6edc
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-07 19:02:33 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -58,9 +58,9 @@ else:
                     st.error("The response is not a valid JSON string.")
             else:
                 st.error("No choices found in the OpenAI response.")
-        except Exception as e:
-            # Handle any exception here
-            st.error(f"An error occurred: {e}")
+    except Exception as e:
+        # Handle any exception her
+        st.error(f"An error occurred: {e}")
 
 
         st.write("Lyrics have been successfully generated.")
