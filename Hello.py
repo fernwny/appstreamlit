97e3c53b226a792c437b0f526b35edb2a5048ee4
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-08 19:51:43 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,10 +10,11 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """Act as a lyricist and write a song about a topic of your choice. The song should be at least 2 verses long.The song should be about the topic you choose. The song should have at least 3 words that have the same vowel sound.
-list the topic, first verse and sceond verse of the song in a JSON array.
+list the topic, first verse and sceond verse and the vocabulary of the song in a JSON array.
 -first line should be the topic
 -second line should be the first verse of the song about the topic
--third line should be the second verse of the song about the topic """
+-third line should be the second verse of the song about the topic 
+-fourth line should be the vocabulary with the meaning of the song"""
 
 st.title("Lyricist")
 st.markdown("This app uses the OpenAI API to generate lyrics based on a topic of your choice.")
@@ -45,7 +46,7 @@ if st.button('Submit'):
 
     # Show the vocabulary to the user and make it only show the vocabulary and the meaning without the index and brackets
     st.markdown('**Vocabulary:**')
-    st.write(sd[2])
+    st.write(sd[3])
 
    
 
