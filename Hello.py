ef3b5488e0713ec49706400ffb56df11d08ab19f
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-08 19:53:22 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -46,7 +46,7 @@ if st.button('Submit'):
 
     # Show the vocabulary to the user and make it only show the vocabulary and the meaning without the index and brackets
     st.markdown('**Vocabulary:**')
-    st.write(sd[2])
+    st.write(sd[3])
 
    
 
