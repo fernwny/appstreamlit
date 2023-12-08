7d6a142197f3f44ffd9273be717cadec112159b5
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-08 20:36:50 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,8 +18,8 @@ list the topic, first verse to tenth verse but the sixth verse repeat the topic 
 -sixth line should be the topic repeated 3 times
 -seventh line should be the fifth verse of the song about the topic
 -eighth line should be the sixth verse of the song about the topic
--ninth line should be the topic repeated 3 times
-and it should have the vocabulary with the meaning below the song in a JSON array.
+-ninth line should be the seventh verse of the song about the topic
+and it should have the table of vocabulary and meaning in a JSON array.
 
 """
 
@@ -51,13 +51,10 @@ if st.button('Submit'):
     print(suggestion_df)
     st.table(suggestion_df)
 
-    # Show the vocabulary with the meaning to the user
-    st.markdown('**Vocabulary:**')
-    st = json.loads(suggestion_dictionary)
-    st.write(st)
 
 
 
 
    
+
 
