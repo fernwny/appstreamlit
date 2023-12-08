1d9e0e7a6411ad7cdaaf86787828b03bdb79fd55
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-08 20:40:37 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,7 +15,7 @@ list the topic, first verse to tenth verse but the sixth verse repeat the topic 
 -third line should be the second verse of the song about the topic 
 -fourth line should be the third verse of the song about the topic
 -fifth line should be the fourth verse of the song about the topic
--sixth line should include the topic 
+-sixth line should include the topic in the verse
 -seventh line should be the fifth verse of the song about the topic
 -eighth line should be the sixth verse of the song about the topic
 -ninth line should be the seventh verse of the song about the topic
