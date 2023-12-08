4811baa3b5a2c17847fb1266c175e3d7a7487fe3
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-08 21:43:01 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,7 +10,7 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """Act as a lyricist and write a song about a topic of your choice. The song should be at least 2 verses long.The song should be about the topic you choose. The song should have at least 3 words that have the same vowel sound.
-list the topic, 8 verses and list the vocabulary and the meaning in the song in a JSON array without index.
+list the topic, first verse to tenth verse but the sixth verse repeat the topic and the vocabulary of the song in a JSON array without index.
 -second line should be the first verse of the song about the topic
 -third line should be the second verse of the song about the topic 
 -fourth line should be the third verse of the song about the topic
@@ -19,7 +19,7 @@ list the topic, 8 verses and list the vocabulary and the meaning in the song in 
 -seventh line should be the fifth verse of the song about the topic
 -eighth line should be the sixth verse of the song about the topic
 -ninth line should be the seventh verse of the song about the topic
--tenth line should be the vocabulary and the meaning
+-tenth line should be the vocabulary with the meaning
 
 """
 
