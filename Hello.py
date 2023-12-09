2e8a2a78439b618059b2fd1b8296ecd0db400462
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-09 20:45:07 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -68,13 +68,17 @@ if st.button('Let\'s go!'):
  
     # Show the vocabulary to the user
     st.markdown('**Vocabulary:**')
-    vocab = sd[9]
-    if isinstance(vocab, dict):
-        for i, (word, meaning) in enumerate(vocab.items(), 1):
-            st.write(f"{i}. {word} : {meaning}")
+    if len(sd) > 9:
+        vocab = sd[9]
+        if isinstance(vocab, dict):
+            for i, (word, details) in enumerate(vocab.items(), 1):
+                for detail in details:
+                    word_type, meaning = detail
+                    st.write(f"{i}. {word} ({word_type}) - {meaning}")
+        else:
+            vocab_str = str(vocab).strip('[]').replace(',', '\n')
+            vocab_list = vocab_str.split('\n')
+            for i, item in enumerate(vocab_list, 1):
+                st.write(f"{i}. {item.strip()}")
     else:
-        vocab_str = str(vocab).strip('[]').replace(',', '\n')
-        vocab_list = vocab_str.split('\n')
-        for i, item in enumerate(vocab_list, 1):
-            st.write(f"{i}. {item.strip()}")
-
+        st.error("The response does not contain a vocabulary list.")
