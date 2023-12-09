08d04acbf2fa23985df2ae70081ff24b20fba7cc
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-09 19:17:56 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -20,7 +20,7 @@ list the first verse to ninth and the vocabulary of the song in a JSON array wit
 -seventh line should be the seventh verse
 -eighth line should be the eighth verse
 -ninth line should be the ninth verse
--tenth line should be the vocabulary in the verses with the type of word and meaning 
+-tenth line should be the vocabulary in the verses with the type in bracket of word and meaning 
 
 """
 
