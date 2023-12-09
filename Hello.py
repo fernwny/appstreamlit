b472aad462c0ac156dacf7c5c03c4397a9d4d32f
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-09 19:22:51 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -69,7 +69,9 @@ if st.button('Submit'):
     vocab = sd[9]
     if isinstance(vocab, dict):
         for i, (word, meaning) in enumerate(vocab.items(), 1):
-            st.write(f"{i}. {word} -{meaning}")
+            for type,mean in meaning:
+                
+                st.write(f"{i}. {word} {type} -{mean}")
     else:
         vocab_str = str(vocab).strip('[]').replace(',', '\n')
         vocab_list = vocab_str.split('\n')
