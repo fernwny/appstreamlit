0a31f76861237989a290951bf17442b010228355
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-09 21:52:52 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -72,9 +72,4 @@ if st.button('Let\'s go!'):
     if isinstance(vocab, dict):
         for i, (word, meaning) in enumerate(vocab.items(), 1):
             st.write(f"{i}. {word} : {meaning}")
-    else:
-        vocab_str = str(vocab).strip('[]').replace(',', '\n')
-        vocab_list = vocab_str.split('\n')
-        for i, item in enumerate(vocab_list, 1):
-            st.write(f"{i}. {item.strip()}")
 
