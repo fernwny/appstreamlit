b68c51e783eb716c66b9f3a5ce2820ea3c5bd1a0
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-09 19:50:12 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -20,16 +20,16 @@ list the first verse to ninth and the vocabulary of the song in a JSON array wit
 -seventh line should be the seventh verse
 -eighth line should be the eighth verse
 -ninth line should be the ninth verse
--tenth line should be the vocabulary in the verses with the type in bracket and meaning 
+-tenth line should be the vocabulary in the verses with the type in bracket of word and meaning 
 
 """
 
-st.title("Auto Lyricist")
+st.title("Mai Lyricist")
 st.markdown("This app uses the OpenAI API to generate lyrics based on keywords of your choice.")
 st.write("Keywords are words that you want to be included in the lyrics. For example, if you want to generate lyrics about love, you can enter the word 'love' as a keyword. The app will then generate lyrics that include the word 'love'")
 
 # Get the topic from the user
-topic = st.text_input("Topic", "ENTER TOPIC HERE (e.g. love, happiness, etc.)")
+topic = st.text_input("Topic", "ENTER TOPIC HERE (e.g. love, heartbreak, etc.")
 
 
 if st.button('Submit'):
@@ -69,6 +69,10 @@ if st.button('Submit'):
     vocab = sd[9]
     if isinstance(vocab, dict):
         for i, (word, meaning) in enumerate(vocab.items(), 1):
-            st.write(f"{i}. {word} - {meaning}")
+            st.write(f"{i}. {word} -{meaning}")
     else:
-        st.error("The response is not a dictionary.")
+        vocab_str = str(vocab).strip('[]').replace(',', '\n')
+        vocab_list = vocab_str.split('\n')
+        for i, item in enumerate(vocab_list, 1):
+            st.write(f"{i}. {item.strip()}")
+
