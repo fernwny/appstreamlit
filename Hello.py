831ec6d203ac89e43145f544342911031c303cdf
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-10 10:11:04 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -59,18 +59,16 @@ if st.button('Submit'):
  
     # Show the vocabulary to the user
     st.markdown('**Vocabulary:**')
+
     vocab = sd[9]
     if isinstance(vocab, dict):
         for i, (word, meaning) in enumerate(vocab.items(), 1):
             st.write(f"{i}. {word} : {meaning}")
-        if isinstance(vocab, list):
-            for i, item in enumerate(vocab, 1):
-                st.write(f"{i}. {item.strip()}")
-
+    elif isinstance(vocab, list):
+        for i, item in enumerate(vocab, 1):
+            st.write(f"{i}. {item.strip()}")
     else:
         vocab_str = str(vocab).strip().replace(',', '\n')
         vocab_list = vocab_str.split('\n')
         for i, item in enumerate(vocab_list, 1):
             st.write(f"{i}. {item.strip()}")
-
-    
