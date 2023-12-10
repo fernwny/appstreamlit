e3a3968b3a85602084aca5f676cfc55b64886c6d
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-10 11:27:44 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,8 +22,7 @@ list the first verse to ninth and the vocabulary of the song in a JSON array wit
 -tenth line should be the vocabulary in the verses with type of word in brackets and meaning
 """
 st.title("MAI Lyricist")
-st.markdown("This app uses the OpenAI API to generate lyrics based on topics of your choice.\n\
-             The lyrics will start with the first verse and end with the eighth verse. The vocabulary of the song will be listed at the end of the song.")
+st.markdown("This app uses the OpenAI API to generate lyrics based on topics of your choice.")
 st.markdown("Example : fog fork ")
 # Get the topic from the user
 topic = st.text_input("Topic", "ENTER TOPIC HERE")
