a47faafc44590f4a21fa322f24a325212f406b71
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-10 10:57:58 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,12 +1,15 @@
+
 import streamlit as st
 import openai
 import json
 import pandas as pd
+
+
 # Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as a lyricist and write a song about a topic of your choice.The song should be about the topic you choose. The song should have at least 3 words that have the same vowel sound.
-list the first verse to ninth and the vocabulary of the song in a JSON array
+prompt = """Act as a lyricist and write a song about a topic of your choice. The song should be at least 2 verses long.The song should be about the topic you choose. The song should have at least 3 words that have the same vowel sound.
+list the first verse to ninth and the vocabulary of the song in a JSON array without index.
 -first line should be the first verse
 -second line should be the second verse
 -third line should be the third verse
@@ -16,18 +19,13 @@ list the first verse to ninth and the vocabulary of the song in a JSON array
 -seventh line should be the seventh verse
 -eighth line should be the eighth verse
 -ninth line should be the ninth verse
--tenth line should be the vocabulary in the song with the type in the bracket and meaning 
+-tenth line should be the vocabulary in the verses with type of word and meaning
 """
-st.title("Mai Lyricist")
-st.markdown("This app uses the OpenAI API to generate lyrics based on keywords of your choice.")
-st.write("Keywords are words that you want to be included in the lyrics. For example, if you want to generate lyrics about love, you can enter the word 'love' as a keyword. The app will then generate lyrics that include the word 'love'")
-
-st.write('Example: dog cat ')
+st.title("Lyricist")
+st.markdown("This app uses the OpenAI API to generate lyrics based on a topic of your choice.")
 # Get the topic from the user
 topic = st.text_input("Topic", "ENTER TOPIC HERE")
-
-
-if st.button('Let\'s go'):
+if st.button('Submit'):
     messages_so_far = [
         {"role": "system", "content": prompt},
         {'role': 'user', 'content': topic},
@@ -40,32 +38,29 @@ if st.button('Let\'s go'):
     st.markdown('**Lyrics:**')
     suggestion_dictionary = response.choices[0].message.content
     sd = json.loads(suggestion_dictionary)
-    if isinstance(sd, list) :
+    if isinstance(sd, list):
         for i, verse in enumerate(sd[:-1], 1):  # Exclude the last item (vocab)
             if i == 5:
                 st.write(f"Chorus: {verse}")
-            elif i >= 6:
-                st.write(f"Verse {i-1}: {verse}")
             else:
                 st.write(f"Verse {i}: {verse}")
     else:
         st.error("The response is not a list.")
     
     
+ 
     print (sd)
     suggestion_df = pd.DataFrame.from_dict(sd)
     print(suggestion_df)
  
     # Show the vocabulary to the user
     st.markdown('**Vocabulary:**')
-    vocab = sd[-1]
+    vocab = sd[9]
     if isinstance(vocab, dict):
         for i, (word, meaning) in enumerate(vocab.items(), 1):
-            st.write(f"{i}. {word} : {meaning}")
+            st.write(f"{i}. {word} - {meaning}")
     else:
-        vocab_str = str(vocab).strip().replace(',', '\n')
+        vocab_str = str(vocab).strip('[]').replace(',', '\n')
         vocab_list = vocab_str.split('\n')
         for i, item in enumerate(vocab_list, 1):
             st.write(f"{i}. {item.strip()}")
-
-    
