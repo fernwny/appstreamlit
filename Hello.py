5c1afcb1bf687ea66f3625ec013ed4878e822d17
fernwny <149886431+fernwny@users.noreply.github.com>
2023-12-10 10:37:20 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -64,6 +64,7 @@ if st.button('Submit'):
     print (sd)
     suggestion_df = pd.DataFrame.from_dict(sd)
     print(suggestion_df)
+    st.table(suggestion_df)
  
     # Show the vocabulary to the user
     st.markdown('**Vocabulary:**')
